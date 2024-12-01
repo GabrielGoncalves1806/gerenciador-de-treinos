606ded7682bb3093219f031412d175274e9702af
GabrielGoncalves1806 <gabrielgoncalves1806@gmail.com>
2024-12-01 00:03:28 -0300

    Adição dos cards de treinos

--- testes/testes.py
+++ testes/testes.py
@@ -1,33 +1,46 @@
 import flet as ft
 from random import choice
 
+
 def main(page:ft.Page):
     cores = [
-        ft.colors.AMBER,
-        ft.colors.GREEN,
-        ft.colors.RED,
-        ft.colors.PURPLE,
-        ft.colors.PINK,
+        ft.Colors.AMBER,
+        ft.Colors.RED,
+        ft.Colors.GREEN,
+        ft.Colors.PURPLE,
+        ft.Colors.PINK,
     ]
+
     cards = ft.Row(expand=1, wrap=False, scroll="always")
-    for x in range(1,5):
+    for x in range(1,6):
+        cor = choice(cores)
+        cores.remove(cor)
         cards.controls.append(
             ft.Container(
-            content=ft.Column(
-                [
-                    ft.Text(f'Treino de Peito {x}'),
-                    
-                ]
-            ),
-            bgcolor=choice(cores),
-            border_radius=10,
-            width=200,
-            height=200,
-            padding=10
-        )
+                content=ft.Column(
+                    [
+                        ft.Text(f'Treino de Peito {x}',size=20),
+                        ft.Image(src='../assets/body_parts/costas.png',width=100,height=100),
+                        ft.ElevatedButton(text='Detalhes',color=cor)
+                    ],
+                    horizontal_alignment=ft.CrossAxisAlignment.CENTER
+                ),
+                bgcolor=cor,
+                border_radius=10,
+                width=200,
+                height=200,
+                padding=10,
+                
+            )
         )
     page.add(
-        cards
+        ft.Column(
+            [
+                ft.Text('Sugestão de treino'),
+                cards
+            ]
+        )
+        
     )
 
 ft.app(main)
